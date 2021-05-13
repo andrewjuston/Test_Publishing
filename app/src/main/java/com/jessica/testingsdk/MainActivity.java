1ac123421d8837c2e686ece87625431a827a8a06
Jessica <>
2021-05-13 16:06:36 +0700

    change example implementation with @aar and change source url, next prepare for Cashlez SDK implementation based on this

--- app/src/main/java/com/jessica/testingsdk/MainActivity.java
+++ app/src/main/java/com/jessica/testingsdk/MainActivity.java
@@ -3,12 +3,19 @@ package com.jessica.testingsdk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.jessica.mylibrary.TesterObject;
 
 public class MainActivity extends AppCompatActivity {
+    TesterObject test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test = new TesterObject();
+        String halooo = test.echo("Halo halo");
+        Toast.makeText(this, halooo, Toast.LENGTH_SHORT).show();
     }
 }
